8b68d4ff97ebf1193a6193f0e0d3b0943f1775a7
ginloy <liyongleow@gmail.com>
2023-07-19 21:17:28 +0800

    idk

--- pwm_sim/pwm_sim.ino
+++ pwm_sim/pwm_sim.ino
@@ -3,13 +3,14 @@
 #include "StaticThreadController.h"
 
 #define PWM_PIN 9
-#define PERIOD 1100
-#define LOWEST 0
+#define PERIOD 20000
+#define LOWEST 1100
 #define HIGHEST 1880
-#define LED_INTERVAL 20
+#define STABLE_PULSE_WIDTH 1500
+#define SWITCH_DELAY 10
+const unsigned long STABLE_DUTY_CYCLE = round(STABLE_PULSE_WIDTH / 20000.0 * 1023.0);
 const unsigned long AMPLITUDE = (HIGHEST - LOWEST) / 2;
 const unsigned long VERTICAL_DISPLACEMENT = HIGHEST - AMPLITUDE;
-
 
 bool START = false;
 unsigned long WAVE_PERIOD = 2000;
@@ -25,12 +26,13 @@ void setup() {
   pinMode(PWM_PIN, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
   Timer1.initialize(PERIOD);
+  Timer1.pwm(PWM_PIN, STABLE_DUTY_CYCLE);
   Serial.begin(9600);
   pwm_update_thread.setInterval(20);
   pwm_update_thread.onRun(update_duty_cycle);
   io_thread.setInterval(20);
   io_thread.onRun(io);
-  led_thread.setInterval(200);
+  led_thread.setInterval(100);
   led_thread.onRun(blink_led);
 }
 
@@ -50,25 +52,38 @@ void io() {
       return;
     }
     START = true;
-    Timer1.pwm(PWM_PIN, DUTY_CYCLE);
+
+    DUTY_CYCLE = round(WAVE_PERIOD / 20000.0 * 1023);
+    Serial.println(DUTY_CYCLE);
+
+    Timer1.setPwmDuty(PWM_PIN, DUTY_CYCLE);
+    // Timer1.setPwmDuty(PWM_PIN, DUTY_CYCLE);
     return;
   }
   String inp = Serial.readString();
+  int temp = inp.toInt();
+  if (temp != 0) {
+    Timer1.setPwmDuty(PWM_PIN, STABLE_DUTY_CYCLE);
+    delay(SWITCH_DELAY);
+    Timer1.setPwmDuty(PWM_PIN, round(temp / 20000.0 * 1023.0));
+    return;
+  }
   inp.trim();
   if (inp == "q") {
     START = false;
-    Timer1.disablePwm(PWM_PIN);
+    Timer1.setPwmDuty(PWM_PIN, STABLE_DUTY_CYCLE);
   }
 }
 
 void update_duty_cycle() {
-  if (!START) {
-    return;
-  }
-  double pulse_width = AMPLITUDE * sin(2 * PI / WAVE_PERIOD * millis()) + VERTICAL_DISPLACEMENT;
-  Serial.println(pulse_width);
-  DUTY_CYCLE = round(pulse_width / PERIOD * 1023.0);
-  Timer1.setPwmDuty(PWM_PIN, DUTY_CYCLE);
+  // if (!START) {
+  //   return;
+  // }
+  // double pulse_width = AMPLITUDE * sin(2 * PI / WAVE_PERIOD * millis()) + VERTICAL_DISPLACEMENT;
+  // Serial.println(pulse_width);
+  // DUTY_CYCLE = round(pulse_width / PERIOD * 1023.0);
+  // // Serial.println(DUTY_CYCLE);
+  // Timer1.setPwmDuty(PWM_PIN, DUTY_CYCLE);
 }
 
 void blink_led() {
