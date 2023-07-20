5c4d8c513895ef0f7b5ce6e784a3d3e534cb37da
ginloy <liyongleow@gmail.com>
2023-07-20 12:09:48 +0800

    cleanup

--- pwm_sim/pwm_sim.ino
+++ pwm_sim/pwm_sim.ino
@@ -4,22 +4,18 @@
 
 #define PWM_PIN 9
 #define PERIOD 20000
-#define LOWEST 1100
-#define HIGHEST 1880
 #define STABLE_PULSE_WIDTH 1500
-#define SWITCH_DELAY 10
-const unsigned long STABLE_DUTY_CYCLE = round(STABLE_PULSE_WIDTH / 20000.0 * 1023.0);
-const unsigned long AMPLITUDE = (HIGHEST - LOWEST) / 2;
-const unsigned long VERTICAL_DISPLACEMENT = HIGHEST - AMPLITUDE;
+#define SWITCH_DELAY 20
+#define FULL_DUTY 1023
+
+unsigned long calculate_duty_cycle(unsigned long pulse_width_micros);
+const unsigned long STABLE_DUTY_CYCLE = calculate_duty_cycle(STABLE_PULSE_WIDTH);
 
 bool START = false;
-unsigned long WAVE_PERIOD = 2000;
-unsigned long DUTY_CYCLE = 0;
 
-Thread pwm_update_thread = Thread();
 Thread io_thread = Thread();
 Thread led_thread = Thread();
-StaticThreadController<3> controller(&pwm_update_thread, &io_thread, &led_thread);
+StaticThreadController<2> controller(&io_thread, &led_thread);
 
 void setup() {
   // put your setup code here, to run once:
@@ -28,8 +24,6 @@ void setup() {
   Timer1.initialize(PERIOD);
   Timer1.pwm(PWM_PIN, STABLE_DUTY_CYCLE);
   Serial.begin(9600);
-  pwm_update_thread.setInterval(20);
-  pwm_update_thread.onRun(update_duty_cycle);
   io_thread.setInterval(20);
   io_thread.onRun(io);
   led_thread.setInterval(100);
@@ -46,26 +40,26 @@ void io() {
   }
   if (!START) {
     String inp = Serial.readString();
-    WAVE_PERIOD = inp.toInt();
-    if (WAVE_PERIOD <= 0) {
+    long pulse_width = inp.toInt();
+    if (pulse_width <= 0 || pulse_width > PERIOD) {
       Serial.println("Invalid input");
       return;
     }
     START = true;
 
-    DUTY_CYCLE = round(WAVE_PERIOD / 20000.0 * 1023);
-    Serial.println(DUTY_CYCLE);
-
-    Timer1.setPwmDuty(PWM_PIN, DUTY_CYCLE);
-    // Timer1.setPwmDuty(PWM_PIN, DUTY_CYCLE);
+    Timer1.setPwmDuty(PWM_PIN, calculate_duty_cycle(pulse_width));
     return;
   }
   String inp = Serial.readString();
-  int temp = inp.toInt();
-  if (temp != 0) {
+  long pulse_width = inp.toInt();
+  if (pulse_width != 0) {
+    if (pulse_width > 1023) {
+      Serial.println("Invalid input");
+      return;
+    }
     Timer1.setPwmDuty(PWM_PIN, STABLE_DUTY_CYCLE);
     delay(SWITCH_DELAY);
-    Timer1.setPwmDuty(PWM_PIN, round(temp / 20000.0 * 1023.0));
+    Timer1.setPwmDuty(PWM_PIN, calculate_duty_cycle(pulse_width));
     return;
   }
   inp.trim();
@@ -75,21 +69,14 @@ void io() {
   }
 }
 
-void update_duty_cycle() {
-  // if (!START) {
-  //   return;
-  // }
-  // double pulse_width = AMPLITUDE * sin(2 * PI / WAVE_PERIOD * millis()) + VERTICAL_DISPLACEMENT;
-  // Serial.println(pulse_width);
-  // DUTY_CYCLE = round(pulse_width / PERIOD * 1023.0);
-  // // Serial.println(DUTY_CYCLE);
-  // Timer1.setPwmDuty(PWM_PIN, DUTY_CYCLE);
-}
-
 void blink_led() {
   if (!START) {
     digitalWrite(LED_BUILTIN, LOW);
     return;
   }
   digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN) ^ 1);
+}
+
+unsigned long calculate_duty_cycle(unsigned long pulse_width_micros) {
+  return round((float)pulse_width_micros / PERIOD * FULL_DUTY);
 }
